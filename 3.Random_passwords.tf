b75d6fc688b116820040eff27ec3bb571174e41e
iakhil31 <akhil.iam3113@gmail.com>
2023-12-26 21:35:26 -0600

    Modified files

--- 3.Random_passwords.tf
+++ 3.Random_passwords.tf
@@ -30,3 +30,25 @@ resource "random_password" "password3" {
   override_special = "!#*?"
   depends_on = [random_password.password1]
 }
+
+resource "random_password" "password4" {
+  length           = 16
+  min_lower        = 4
+  min_upper        = 4
+  min_numeric      = 4
+  min_special      = 4
+  special          = true
+  override_special = "!#*?"
+  depends_on = [random_password.password3]
+}
+
+resource "random_password" "password5" {
+  length           = 16
+  min_lower        = 4
+  min_upper        = 4
+  min_numeric      = 4
+  min_special      = 4
+  special          = true
+  override_special = "!#*?"
+  depends_on = [random_password.password4]
+}
