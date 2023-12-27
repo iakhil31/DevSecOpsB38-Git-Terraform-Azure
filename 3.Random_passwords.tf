e0e0813302d0be4ce77398a26c59ff4e9c1591f7
iakhil31 <akhil.iam3113@gmail.com>
2023-12-26 21:22:23 -0600

    Modifid passwords

--- 3.Random_passwords.tf
+++ 3.Random_passwords.tf
@@ -6,6 +6,7 @@ resource "random_password" "password1" {
   min_special      = 4
   special          = true
   override_special = "!#*?"
+  depends_on = [azurerm_subnet.subnet3]
 }
 
 resource "random_password" "password2" {
@@ -16,4 +17,16 @@ resource "random_password" "password2" {
   min_special      = 4
   special          = true
   override_special = "!#*?"
+ depends_on = [random_password.password1] 
+}
+
+resource "random_password" "password3" {
+  length           = 16
+  min_lower        = 4
+  min_upper        = 4
+  min_numeric      = 4
+  min_special      = 4
+  special          = true
+  override_special = "!#*?"
+  depends_on = [random_password.password1]
 }
